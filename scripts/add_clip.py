f4a932a114908d8c3cc7fda95390890d659cfcbd
Tully Foote <tfoote@osrfoundation.org>
2020-04-11 16:33:40 -0700

    pull the title from youtube automatically
    Signed-off-by: Tully Foote <tfoote@osrfoundation.org>

--- scripts/add_clip.py
+++ scripts/add_clip.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 
-
+import argparse
 import em
 import pathlib
 
 
-title = input("Plese enter a title: ")
+from lxml import html
+import requests
 
-clip_id = input("Please enter the clip ID: ")
-clip_shortname = input("Please enter the clip shortname, lowercase_underscored: ")
-is_tutorial = input("Is this a tutorial [y/N]?").lower().startswith('y')
+
+parser = argparse.ArgumentParser(description='Generate Clip Metadata')
+parser.add_argument('clip_id', nargs='?', default=None)
+
+args = parser.parse_args()
+
+clip_info = {}
+
+if not args.clip_id:
+    clip_info['clip_id'] = input("Plese enter a youtube id: ")
+else:
+    clip_info['clip_id'] = args.clip_id
+
+url = 'https://youtube.com/watch?v=%s' % args.clip_id
+
+page = requests.get(url)
+
+tree = html.fromstring(page.content)
+
+
+
+clip_info['title'] = tree.xpath('/html/head/title')[0].text
+
+clip_info['clip_shortname'] = input("Please enter the clip shortname, lowercase_underscored: ")
+clip_info['is_tutorial'] = input("Is this a tutorial [y/N]?").lower().startswith('y')
 
 TEMPLATE="""---
 layout: post
@@ -23,7 +46,7 @@ clip_id: '@(clip_shortname)'
 
 """
 
-short_filename = '%s.md' % clip_shortname
+short_filename = '%s.md' % clip_info['clip_shortname']
 
 clip_file = pathlib.Path(__file__).parents[1].joinpath('_clips').joinpath(short_filename).absolute()
 
@@ -34,10 +57,10 @@ print("Creating file: ", clip_file)
 
 print("contents")
 
-out = em.expand(TEMPLATE, locals())
+out = em.expand(TEMPLATE, clip_info)
 print(out)
 
 with open(clip_file, 'w') as fh:
     fh.write(out)
 
-print('Please reference it with name %s' % clip_shortname)
+print('Please reference it with name %s' % clip_info['clip_shortname'])
