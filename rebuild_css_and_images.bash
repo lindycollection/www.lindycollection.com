27ab52e435a635d68d03de29b99c0bfc659b9362
Tully Foote <tully.foote@gmail.com>
2023-10-08 13:21:40 -0700

    Upgrading node itself
    And bumping all dependencies to the latest
    
    https://github.com/nodejs/node/issues/38367

--- rebuild_css_and_images.bash
+++ rebuild_css_and_images.bash
@@ -2,8 +2,13 @@
 
 set -e
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+export NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
 sudo apt-get update && sudo apt-get -y install npm
-npm install .
+CXXFLAGS="--std=c++17" npm install .
 
 node_modules/gulp-cli/bin/gulp.js sass
 node_modules/gulp-cli/bin/gulp.js img
