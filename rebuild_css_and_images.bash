83c4af4a66ce70ddba7b4566b84dc736aa8a3931
Tully Foote <tully.foote@gmail.com>
2023-10-09 02:01:42 -0700

    no need for newer version of node it actually conflicts Also making the script invokable from other paths.

--- rebuild_css_and_images.bash
+++ rebuild_css_and_images.bash
@@ -2,12 +2,10 @@
 
 set -e
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-export NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 sudo apt-get update && sudo apt-get -y install npm
+cd $SCRIPT_DIR
 CXXFLAGS="--std=c++17" npm install .
 
 node_modules/gulp-cli/bin/gulp.js sass
